635e445fc785dd27890c2b4d1ce1646c703286d7
Alexey_Makeev <makeevhome@mail.ru>
2023-10-01 17:09:54 +0300

    seminar10

---
+++ Seminar10/data.js
@@ -0,0 +1,64 @@
+const dataObj = {
+  cards: [
+    {
+      code: "3C",
+      image: "3C.png",
+      value: "3",
+      suit: "CLUBS",
+    },
+    {
+      code: "9H",
+      image: "9H.png",
+      value: "9",
+      suit: "HEARTS",
+    },
+    {
+      code: "AD",
+      image: "AD.png",
+      value: "ACE",
+      suit: "DIAMONDS",
+    },
+    {
+      code: "KC",
+      image: "KC.png",
+      value: "KING",
+      suit: "CLUBS",
+    },
+    {
+      code: "6H",
+      image: "6H.png",
+      value: "6",
+      suit: "HEARTS",
+    },
+    {
+      code: "2D",
+      image: "2D.png",
+      value: "2",
+      suit: "DIAMONDS",
+    },
+    {
+      code: "2S",
+      image: "2S.png",
+      value: "2",
+      suit: "SPADES",
+    },
+    {
+      code: "7S",
+      image: "7S.png",
+      value: "7",
+      suit: "SPADES",
+    },
+    {
+      code: "QD",
+      image: "QD.png",
+      value: "QUEEN",
+      suit: "DIAMONDS",
+    },
+    {
+      code: "4D",
+      image: "4D.png",
+      value: "4",
+      suit: "DIAMONDS",
+    },
+  ],
+};
